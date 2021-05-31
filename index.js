e1d8bb6530f9674789252571889776d7e499aeb6
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-05-31 00:08:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,32 @@ const { command } = require('../pc-commands/commands/echo');
 module.exports = class Linkshortner extends Plugin {
     startPlugin () {
         powercord.api.commands.registerCommand({
-            command: "Link Shortner",
-            aliases: [ "Url Shortner", "Url Shortner", "UrlShortner", "Link Shortner", "LinkShortner" ],
+            command: "Link Shortner is.gd",
+            aliases: [ "Url Shortner", "Url Shortner", "UrlShortner", "is.gd" ],
             description: "Lets you shrink urls using is.gd",
             usage: "{c}LinkShortner <url>",
-            executor: this.linkshortner.bind(this),
-            autocomplete: args => {}, // shows auto complete
+            executor: this.linkshortneris.bind(this)
+        });
+        powercord.api.commands.registerCommand({
+            command: "Link Shortner v.gd",
+            aliases: [ "Url Shortner", "Url Shortner", "v.gd" ],
+            description: "Lets you shrink urls using v.gd",
+            usage: "{c}LinkShortner <url>",
+            executor: this.linkshortnerv.bind(this)
         });
     }
     pluginWillUnload() {
         powercord.pluginWillUnload('Linkshortner')
     }
-    async linkshortner(args) {
+    async linkshortneris(args) {
         const data = await get("https://is.gd/create.php?format=simple&url="+args[0]);
+        return {
+            send: true,
+            result: `${data.body}`
+        };
+    }
+    async linkshortnerv(args) {
+        const data = await get("https://v.gd/create.php?format=simple&url="+args[0]);
         return {
             send: true,
             result: `${data.body}`
